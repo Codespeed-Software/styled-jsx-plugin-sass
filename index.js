632ecd6aa057576337a7bec2e39bbbce7501a513
Dallas James <dallasjames42@gmail.com>
2024-11-20 15:38:10 -0700

    update to silence warnings for now

--- index.js
+++ index.js
@@ -23,23 +23,24 @@ module.exports = (css, settings) => {
   // Prepend option data to cssWithPlaceholders
   const optionData = (settings.sassOptions && settings.sassOptions.data) || ''
   // clean up extra indent (indentedSyntax is not compatible with extra indenting)
-  // they need to be cleaned up separately, and than concated
+  // they need to be cleaned up separately, and then concatenated
   const data = stripIndent(optionData) + '\n' + stripIndent(cssWithPlaceholders)
   const file = settings.babel && settings.babel.filename
 
   const preprocessed = sass
     .renderSync(
       Object.assign(
-        {}, 
-        { file },
-        settings.sassOptions, 
-        {loader: 'sass-loader'},
-        {implementation: 'sass-embedded'},
-        {style: 'compressed'},
-        {api: 'modern-compiler'},
-        {silenceDeprecations: ['legacy-js-api']},
-        { data }
-      )).css.toString()
+        {},
+        settings.sassOptions,
+        {
+          file,
+          silenceDeprecations: ['legacy-js-api'],
+          data
+        },
+      )
+    )
+    .css.toString()
+  // .compileString(data, {loadPaths: ['styles/', 'core/styles/'], includePaths: ['styles/', 'core/styles/']}) // should switch to this soon
 
   return preprocessed
     .replace(
